7e3e080906d6140c6c5cb7c0474cc244fc571f91
24fi076 <miomame.17@gmail.com>
2026-06-12 11:55:30 +0900

    new

--- main.py
+++ main.py
@@ -37,13 +37,13 @@ def omikuji():
 @app.get("/index")
 def index():
     html_content = """
-        <html>
-            <head>
-                <title>Some HTML in here</title>
-            </head>
-            <body>
-                <h1>Look at me! HTML!</h1>
-            </body>
-        </html>
-        """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
     return HTMLResponse(content=html_content, status_code=200)
